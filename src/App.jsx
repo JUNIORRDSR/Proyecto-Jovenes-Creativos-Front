97651845fa66330636d049a7f8630255b9f28908
marllelypg <marllelypg@gmail.com>
2025-10-21 16:51:10 -0500

    Basic settings

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Aqui desarrollaremos nuestra primera aplicación Gametracker</h1>
+      <p>¡Hola mundo!</p>
+    </div>
+  )
+}
+
+export default App
