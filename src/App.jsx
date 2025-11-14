ef13cef7e079dc08a6d822eaeca788bf18bd3b15
marllelypg <marllelypg@gmail.com>
2025-11-14 14:57:30 -0500

    fix: corregido error de ratings mayores a 5 al cargar juegos del backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import Library from "./pages/Library.jsx";
@@ -13,7 +13,7 @@ function App() {
       id: 1,
       name: "Cyberpunk 2077",
       genre: "Acción",
-      cover:"https://wallpapers.com/images/hd/cyberpunk-2077-male-and-female-v-63386du3wmsbho7d.webp",
+      cover: "https://wallpapers.com/images/hd/cyberpunk-2077-male-and-female-v-63386du3wmsbho7d.webp",
       rating: 4.5,
       status: "Jugando",
       hoursPlayed: 45,
@@ -22,26 +22,40 @@ function App() {
       id: 2,
       name: "God of War",
       genre: "Aventura",
-      cover:"https://wallpapers.com/images/hd/god-of-war-3d-kratos-n1im6er87u3hisap.webp",
+      cover: "https://wallpapers.com/images/hd/god-of-war-3d-kratos-n1im6er87u3hisap.webp",
       rating: 5,
       status: "Completado",
       hoursPlayed: 25,
     },
   ]);
-  
+
+
   const addGame = (newGame) => setGames((prev) => [...prev, newGame]);
+
   const editGame = (updatedGame) =>
     setGames((prev) =>
       prev.map((g) => (g.id === updatedGame.id ? updatedGame : g))
     );
-  const deleteGame = (id) => setGames((prev) => prev.filter((g) => g.id !== id));
 
-    useEffect(() => {
-    fetch('https://6909419a2d902d0651b32224.mockapi.io/games')
-        .then((response) => response.json())
-        .then((data) => setGames(data))
-        .catch((error) => console.error("Error fetching games:", error));
-    }, []);
+  const deleteGame = (id) =>
+    setGames((prev) => prev.filter((g) => g.id !== id));
+
+
+  useEffect(() => {
+    fetch("https://6909419a2d902d0651b32224.mockapi.io/games")
+      .then((response) => response.json())
+      .then((data) => {
+        // Normalizar rating y hoursPlayed
+        const cleanedGames = data.map((g) => ({
+          ...g,
+          rating: Math.min(Math.max(Number(g.rating) || 0, 0), 5),
+          hoursPlayed: Number(g.hoursPlayed) || 0, 
+        }));
+
+        setGames(cleanedGames);
+      })
+      .catch((error) => console.error("Error fetching games:", error));
+  }, []);
 
   return (
     <Router>
